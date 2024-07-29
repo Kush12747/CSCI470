1e5ea5f4946c4608db0bb2196665864947f964e0
Kush Gandhi <Kushgandhi2099@gmail.com>
2024-07-29 08:47:42 -0500

    Update MileRedeemer.java

--- Assignment 2/MileRedeemer.java
+++ Assignment 2/MileRedeemer.java
@@ -1,13 +1,3 @@
-// *************************************************************************** 
-// 
-//   Kush Gandhi 
-//   Z1968933
-//   CSCI 470-PE1
-//
-//   I certify that this is my own work and where appropriate an extension 
-//   of the starter code provided for the assignment. 
-// ***************************************************************************
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
